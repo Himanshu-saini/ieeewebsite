78c7fc55b1680cd6ba4f5df85fa31dfe77ecfd2e
Himanshu <himanshusaini06@gmail.com>
2019-11-02 00:22:17 +0530

    remove other error in events.models

--- ieeewebsite/apps/events/models.py
+++ ieeewebsite/apps/events/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 import datetime
-from django.utils import timezone
 
 # Create your models here.
 
-status_options = ( 'Completed' , 'Ongoing' , 'Upcoming')
+status_options = ( ('CMP','Completed') , ('ONG','Ongoing') , ('UPC','Upcoming'))
 
 class Events(models.Model):
     Event_Name = models.CharField(max_length = 30,help_text= "Name Of the Event")
     Discription = models.CharField(max_length = 500,help_text= "Small description about the Event",blank=True,null=True)
     Event_Date = models.DateField( default=datetime.date.today())
-    Event_Time =  models.TimeField( default=timezone.now)
+    Event_Time =  models.TimeField()
     Details = models.CharField(max_length = 500,help_text= "Details about the Event",blank=True,null=True)
-    Event_status = models.CharField(choices = status_options,help_text= "Status of the Event",default="Completed")
-    Display_img = models.ImageField(upload_to="/uploads/events/images/CoverImage", help_text = "Cover Image Of the event",blank=True,null=True)
+    Event_status = models.CharField(choices = status_options,max_length = 3,help_text= "Status of the Event",default="Completed")
+    Display_img = models.ImageField(upload_to="uploads/events/images/CoverImage", help_text = "Cover Image Of the event",blank=True,null=True)
 
     def __str__(self):
         return self.Event_Name+" "+str(self.Event_Date)
