81241b2378591b7d81cf954b78f9caf17deb350e
Himanshu <himanshusaini06@gmail.com>
2019-11-01 23:38:53 +0530

    views updates and upls file added and updates

--- ieeewebsite/apps/events/views.py
+++ ieeewebsite/apps/events/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+# from .models import Team
 # Create your views here.
+
+def events(req):
+    return render(req,"team.html",{ 'title':'IEEE Events' })
