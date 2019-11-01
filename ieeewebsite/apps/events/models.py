2b49a3efa167791da3cc26d50442f86272a99fce
Himanshu <himanshusaini06@gmail.com>
2019-11-02 00:14:15 +0530

    Error removed -  datetimme.timezone does not have attribute now'

--- ieeewebsite/apps/events/models.py
+++ ieeewebsite/apps/events/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Events(models.Model):
     Event_Name = models.CharField(max_length = 30,help_text= "Name Of the Event")
     Discription = models.CharField(max_length = 500,help_text= "Small description about the Event",blank=True,null=True)
     Event_Date = models.DateField( default=datetime.date.today())
-    Event_Time =  models.TimeField( default=datetime.timezone.now)
+    Event_Time =  models.TimeField( default=timezone.now)
     Details = models.CharField(max_length = 500,help_text= "Details about the Event",blank=True,null=True)
     Event_status = models.CharField(choices = status_options,help_text= "Status of the Event",default="Completed")
     Display_img = models.ImageField(upload_to="/uploads/events/images/CoverImage", help_text = "Cover Image Of the event",blank=True,null=True)
