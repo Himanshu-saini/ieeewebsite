d69c6ec9024fb4c16391dead12b56696b4418a6f
Himanshu <himanshusaini06@gmail.com>
2019-11-04 19:00:24 +0530

    static files dir changed

--- ieeewebsite/settings.py
+++ ieeewebsite/settings.py
@@ -122,8 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = " "
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'/ieeewebsite/apps/home/static'),
     os.path.join(BASE_DIR,'/ieeewebsite/apps/about/static'),
@@ -135,6 +138,6 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'sass_processor.finders.CssFinder',
 ]
-# SASS_PROCESSOR_URL = "/static/"
-SASS_PROCESSOR_ROOT = '/static/'
+SASS_PROCESSOR_URL = "/static/"
+SASS_PROCESSOR_ROOT = os.path.join(BASE_DIR, 'static')
 
