c92f01af977984674849e6853113b2b86340933f
Himanshu <himanshusaini06@gmail.com>
2019-11-06 01:14:52 +0530

    navigation bar fot mobile improved

--- ieeewebsite/apps/home/static/home/javascripts/header-div.js
+++ ieeewebsite/apps/home/static/home/javascripts/header-div.js
@@ -3,13 +3,14 @@ function showmenu()
     var menu = document.getElementById("menu-icon");
     var arrow = document.getElementById("arrow-icon");
     var x = document.getElementById("nav-list");
-    if (x.style.transform == 'scaleX(1)') {
-        x.style.transform = "scaleX(0)";
+    console.log(x.style.right);
+    if (x.style.right == '0%') {
+        x.style.right = "-60%";
         menu.style.display = "block";
         arrow.style.display = "none";
     } 
     else {
-        x.style.transform = "scaleX(1)";
+        x.style.right = "0%";
         menu.style.display = "none";
         arrow.style.display = "block";
     }
