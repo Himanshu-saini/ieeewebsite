2958a05054ed974c69d4005550a41c816accfb45
Himanshu <himanshusaini06@gmail.com>
2019-11-06 01:24:10 +0530

    mobile navigation bar updated

--- ieeewebsite/apps/home/static/home/javascripts/header-div.js
+++ ieeewebsite/apps/home/static/home/javascripts/header-div.js
@@ -3,14 +3,13 @@ function showmenu()
     var menu = document.getElementById("menu-icon");
     var arrow = document.getElementById("arrow-icon");
     var x = document.getElementById("nav-list");
-    console.log(x.style.right);
-    if (x.style.right == '0%') {
-        x.style.right = "-60%";
+    if (x.style.transform == 'translate(0%, 0%)') {
+        x.style.transform = "translate(100%,0%)";
         menu.style.display = "block";
         arrow.style.display = "none";
     } 
     else {
-        x.style.right = "0%";
+        x.style.transform = "translate(0%,0%)";
         menu.style.display = "none";
         arrow.style.display = "block";
     }
