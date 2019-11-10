af6f28ae427d0b70aa9001d458f4277385b880f9
Himanshu <himanshusaini06@gmail.com>
2019-11-11 00:17:50 +0530

    change the default port of runserver

--- manage.py
+++ manage.py
@@ -6,6 +6,11 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ieeewebsite.settings')
+    
+    # Override default port for `runserver` command
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "80"
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
