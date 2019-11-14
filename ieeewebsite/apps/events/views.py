a897f6337a8f48d2ef7fef76a7594f5d47c5c365
Himanshu <himanshusaini06@gmail.com>
2019-11-14 18:02:22 +0530

    event page editted

--- ieeewebsite/apps/events/views.py
+++ ieeewebsite/apps/events/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-# from .models import Team
+from .models import Event
 # Create your views here.
 
 def events(req):
-    return render(req,"events.html",{ 'title':'IEEE Events','events':range(3) })
+    return render(req,"events.html",{ 'title':'IEEE Events','events':list(Event.objects.all().order_by()) })
