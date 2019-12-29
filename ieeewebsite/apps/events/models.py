136bee874d68cbea24af0c7d2a14b8d8fdcbca59
Himanshu <himanshusaini06@gmail.com>
2019-12-29 21:50:01 +0530

    event page changed

--- ieeewebsite/apps/events/models.py
+++ ieeewebsite/apps/events/models.py
@@ -12,7 +12,7 @@ class Event(models.Model):
     Event_Time =  models.TimeField()
     Details = models.CharField(max_length = 500,help_text= "Details about the Event",blank=True,null=True)
     Event_status = models.CharField(choices = status_options,max_length = 3,help_text= "Status of the Event",default="Completed")
-    Display_img = models.ImageField(upload_to="uploads/events/images/CoverImage", help_text = "Cover Image Of the event",blank=True,null=True)
+    Display_img = models.ImageField(upload_to="events/images/poster", help_text = "Cover Image Of the event",blank=True,null=True)
 
     def __str__(self):
         return self.Event_Name+" "+str(self.Event_Date)
--- ieeewebsite/apps/events/views.py
+++ ieeewebsite/apps/events/views.py
@@ -4,4 +4,4 @@ from .models import Event
 # Create your views here.
 
 def events(req):
-    return render(req,"events.html",{ 'title':'IEEE Events','events':list(Event.objects.all().order_by()),'mevents':range(7) })
+    return render(req,"Event.html",{ 'title':'IEEE Events','events':list(Event.objects.all().order_by()),'mevents':range(7) })
